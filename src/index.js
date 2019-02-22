d8f8cb51fb621504ec9d42b5bbaec4e30c297ccf
Romain GUYOT <guyotr@tetras.univ-savoie.fr>
2019-02-22 11:51:08 +0100

    end commit$

--- src/index.js
+++ src/index.js
@@ -2,10 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // YOUR CODE HERE
+const jimp = require('jimp');
+const lsb = require('lsb');
+const path = require('path');
+var m;
 
-const Button = (props) => <button type="button">{props.name}</button>
+const images = [
+  require('./1.png'),
+  require('./2.png'),
+  require('./3.png'),
+  require('./4.png'),
+  require('./5.png')
+];
 
-ReactDOM.render(
-  <Button name='1'/>,
-  document.getElementById('root')
-);
+async function decode(inputImage) {
+    const image = await jimp.read(inputImage);
+    const json = lsb.decode(image.bitmap.data, rgb);
+    const message = JSON.parse(json);
+    const s = JSON.stringify(message);
+    return s;
+  }
+  
+
+  function rgb(n) {
+    return n + Math.floor(n / 3);
+  }
+
+  const MyCmponent = () => {
+    const imageTags = 
+      images.map(
+        i=><img src={i} />
+      )
+    return <h1>{imageTags}</h1>;
+  }
+
+  const CmponentText = () =>{
+    const imagesmap = 
+      images.map(
+        i => <p>decode(i)</p> 
+      )
+      return <h1>{imagesmap}</h1>
+  }
+
+  ReactDOM.render(
+    <MyCmponent/>,
+    <CmponentText/>,
+    document.getElementById('root')
+  );
